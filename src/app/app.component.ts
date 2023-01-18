65a296ee735115fcf641331065394d24a2eecc0a
kaztro <00229017@uca.edu.sv>
2023-01-18 16:44:32 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CRUD_Angular_Spring_MySQL_JSP';
+}
